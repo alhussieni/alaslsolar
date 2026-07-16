3b9e3fdf1328b8ca939e729a158c2d07daa71d64
alhussieni <alhussieni95@gmail.com>
2026-07-16 22:18:37 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.ALASL_SUPABASE_URL = "https://nymkmrdbicfuniobunth.supabase.co";
-window.ALASL_SUPABASE_ANON_KEY = "sb_publishable_vrHvQTuZT4kbzFP6hoIf0w_5FMFFk2e";
+window.ALASL_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55bWttcmRiaWNmdW5pb2J1bnRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3NzY0MjIsImV4cCI6MjA5NTM1MjQyMn0.31gmIJjgJM6MO0vcZqON-463MjZSe_2kcXUPlxtI5dY";
 
 function getAlaslSupabase() {
   if (!window.supabase) return null;
