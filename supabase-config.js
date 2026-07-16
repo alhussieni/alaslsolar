83c1ecd2a048dddbe59eed6b35d44546b239a3f9
alhussieni <alhussieni95@gmail.com>
2026-07-16 15:10:51 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
 window.ALASL_SUPABASE_URL = "https://nymkmrdbicfuniobunth.supabase.co";
 window.ALASL_SUPABASE_ANON_KEY = "sb_publishable_vrHvQTuZT4kbzFP6hoIf0w_5FMFFk2e";
+
 function getAlaslSupabase() {
   if (!window.supabase) return null;
   if (!window.alaslSupabase) {
